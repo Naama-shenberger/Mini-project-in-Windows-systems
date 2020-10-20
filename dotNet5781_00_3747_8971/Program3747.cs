5aff15e2d3729303a179959e56bca0f38db0e916
נעמה שנברגר <shanberg@g.jct.ac.il>
2020-10-20 21:40:59 +0300

    initial code + Welcome3747

---
+++ dotNet5781_00_3747_8971/Program3747.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3747_8971
+{
+      partial class Program
+      {
+          static void Main(string[] args)
+          {
+                Welcome3747();
+                Welcome8971();
+                Console.ReadKey();
+          }
+           static partial void Welcome8971();
+           private static void Welcome3747()
+           {
+              Console.Write("Enter your name: ");
+              string name = Console.ReadLine();
+              Console.WriteLine("{0}, welcome to my first console application", name);
+           }
+
+       }
+    
+}
