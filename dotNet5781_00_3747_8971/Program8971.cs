fea6f0816b586544f8268b5cc3ab09e45e9c8a30
ella hanzin <hanzin@g.jct.ac.il>
2020-10-20 21:50:56 +0300

    initial code + Welcome8971

---
+++ dotNet5781_00_3747_8971/Program8971.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3747_8971
+{
+    partial class Program
+    {
+        static partial void Welcome8971()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
