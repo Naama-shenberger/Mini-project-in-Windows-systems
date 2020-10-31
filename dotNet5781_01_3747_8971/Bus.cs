451f78376acb67fc20f93aae35267ec90c9b633f
נעמה שנברגר <shanberg@g.jct.ac.il>
2020-10-31 17:44:26 +0200

    Ex#1 Final commit

--- dotNet5781_01_3747_8971/Bus.cs
+++ dotNet5781_01_3747_8971/Bus.cs
@@ -3,65 +3,116 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dotNet5781_01_3747_8971
 {
+    /// <summary>
+    /// Bus class, which allows access to the fields by methods 'set' and 'get'
+    /// Functions that check the integrity and update of buses
+    /// </summary>
     class Bus
     {
-        int[] licensePlate;
-        DateTime dateActivity;
-        float kilometers;
-        bool status;
-        float totalMiles;// נסיעה ק"מ כוללת 
-        public static void Mileage(float _kh)//שמירה הנסיעה הכוללת 
+        public static Random r = new Random();
+        private string l_licensePlate;
+        private DateTime d_dateActivity;
+        private DateTime d_dateTreatment;
+        private float k_kilometersTreatment;
+        private float k_kilometersGas;
+        private float t_totalkilometers;
+        /// <summary>
+        /// set and get functions for private fields
+        /// </summary>
+        public float KILOMETERSGAS
         {
-           this.totalMiles += _kh;
+            get { return k_kilometersGas; }
+            set { k_kilometersGas = value; }
+        }
+        public DateTime DATETREATMET
+        {
+            get { return d_dateTreatment; }
+            set { d_dateTreatment= value; }
+        }
+        public DateTime DATEACTIVITY
+        {
+            get { return d_dateActivity; }
+            set { d_dateActivity = value; }
+        }
+        public float TOTALKILOMETERS
+        {
+            get { return t_totalkilometers; ; }
+            ///private set,doesn't allowed to change the total kilometers
+            private set { t_totalkilometers = value; }
 
         }
+        public float kILOMETERS_TREATMENT
+        {
+            get { return k_kilometersTreatment; }
+            set { k_kilometersTreatment = value; }
+        }
+    
+        public string  LICENSEPLATE
+        {
+            get { return l_licensePlate; }
+            set { l_licensePlate = value; }
+        }
+        /// <summary>
+        /// A function that receives a parameter and adds to the total kilometers
+        /// </summary>
+        /// <param name="_kilometers"></param>
+        public void Mileage(float _kilometers)
+        {
+            this.TOTALKILOMETERS += _kilometers;
+        }
+        /// <summary>
+        /// A function that checks if the vehicle needs to be refueled
+        /// </summary>
+        /// <returns></returns>
         public bool FuelCondition()
         {
-            Bus MyBus = new Bus();
-            if (MyBus.kilometers > 1200)
+            if (this.k_kilometersGas > 1200)
                 return true;
             return false;
         }
+        /// <summary>
+        /// A function that checks how many numbers the user needs to type for the number plate 
+        /// </summary>
+        /// <returns></returns>
         public int NumberOflicensePlate()
         {
-            Bus MyBus = new Bus();
-            int year = int.Parse(MyBus.dateActivity.Year.ToString());
+            int year; 
+            int.TryParse(this.d_dateActivity.Year.ToString(),out year);
             return year < 2018 ? 7 : 8;
         }
+        /// <summary>
+        /// Function that checks if the bus needs treatment - 
+        /// treatment bus defined if the bus has traveled more than 20,000 kilometers or a year has passed since the last treatment
+        /// </summary>
+        /// <returns></returns>
         public bool TreatmentIsNeeded()
         {
-          
-            if (MyBus.kilometers>20000 || 
-        }
+            if (this.k_kilometersTreatment >= 20000 || dateCheck())
+                return true;
+            return false;
+        }/// <summary>
+         /// A function that checks if a year has passed since the last treatment
+         /// </summary>
+         /// <returns></returns>
         bool dateCheck()
         {
-            Bus MyBus = new Bus();
-            int day = int.Parse(MyBus.dateActivity.Day.ToString());
-            int month = int.Parse(MyBus.dateActivity.Month.ToString());
-            int year = int.Parse(MyBus.dateActivity.Year.ToString());
+            int day;
+            int month;
+            int year;
+            int.TryParse(this.d_dateTreatment.Day.ToString(),out day);
+            int.TryParse(this.d_dateTreatment.Month.ToString(),out month);
+            int.TryParse(this.d_dateTreatment.Year.ToString(),out year);
+            DateTime currentDate = DateTime.Now;
+            if (int.Parse(currentDate.Day.ToString()) == day && int.Parse(currentDate.Month.ToString()) == month && int.Parse(currentDate.Year.ToString()) <year || int.Parse(currentDate.Year.ToString()) < year)
+                return true;
+            return false;
         }
 
     }
 }
-
-/* כל אוטובוס בבעלותה מאופיין במספר רישו
- * י. כמו כן צריך לדעת את תאריך תחילת הפעילות
-של כלי התחבורה.
-כדי לשמור על מצב כלי תחבורה תקין יש לבצע טיפול כללי מדי 000,20 ק"מ נסועה, או מדי
-שנה, לפי המוקדם בינהם, יש לשים לב שע"פ התקנות אוטובוס שנסע יותר מ000,20 ק"מ
-מהטיפול האחרון מוגדר מסוכן, ולכן אין לאפשר לאוטובוס לבצע נסיעה בה הוא יעבור את
-מספר הק"מ המותר.
-יש לשמור את הנסועה הכוללת )קילומטראז '( של כלי התחבורה.
-בנוסף יש לשמור מידע על מצב הדלק בכלי התחבורה, אוטובוס יכול לנסוע לכל היותר 1200
-ק"מ אחרי תדלוק, ולכן אין לאפשר לו לבצע נסיעה בה הוא יעבור את מספר הק"מ הזה ללא
-תדלוק קודם.
-הערות:
-• מספר רישוי הוא מספר בן 7 לאוטובוסים שנכנסו לפעילות לפני שנת 2018 ו8 ספרות לשאר.
-דרך ההצגה של מספר רישוי תתבצע בהתאמה לפי הפורמט הבא: 67 - 345 - 12 או 678 - 45 - 123
-• אין לאפשר הפחתה של הנסועה הכוללת של כלי תחבורה*/
-
--- dotNet5781_01_3747_8971/Program.cs
+++ dotNet5781_01_3747_8971/Program.cs
@@ -4,12 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/// <summary>
+/// Exercise 1 - Mini project in c#, Bus system
+/// Name:Naama Shenberger 
+/// Id:211983747
+/// Name:Ella Hanzin
+/// Id:212028971
+/// <summary>
 
 namespace dotNet5781_01_3747_8971
 {
     enum Choices { Exit, Add_Bus, Choose_Bus, Refuel_Treat, Traveled }
-   class Program
-   {
+    class Program
+    {
+        /// <summary>
+        /// A function that asks the user to select an action
+        /// </summary>
         static int ShowChoices()
         {
             Console.WriteLine("Enter your choice of action: ");
@@ -18,45 +28,225 @@ namespace dotNet5781_01_3747_8971
             Console.WriteLine("Click 3 to refuel or treat a bus ");
             Console.WriteLine("Click 4 to show travel of the last treatment for all vehicles in the company ");
             Console.WriteLine("Click 0 to exit the program ");
-            int _choice = int.Parse(Console.ReadLine());
+            int _choice;
+            int.TryParse(Console.ReadLine(), out _choice);
             return _choice;
         }
-    static void Main(string[] args)
-    {
-        List<Bus> buslist = new List<Bus>();
-        int choice = (int)ShowChoices();
-        while (choice != 0)
+        static void Main(string[] args)
         {
-            switch ((Choices)choice)
+            DateTime currentDate = DateTime.Now;
+            ///<summary>
+            ///Function that receives from user a plate number,and Returns it
+            ///<summary>
+            string Licenseplate()
             {
-                case Choices.Add_Bus:
-                    Console.WriteLine("Enter bus license plate: ");
-                    //we need to c how we can do this
-                    Console.WriteLine("Enter bus starting date: ");
-                    break;
-                case Choices.Choose_Bus:
-                    Console.WriteLine("Enter bus license plate: ");
-                    //?
-                    break;
-                case Choices.Refuel_Treat:
-                    Console.WriteLine("Enter bus license plate: ");
-                    //
-                    Console.WriteLine("Click 0 if you want to Refuel");
-                    Console.WriteLine("Click 1 if you want to Treat the bus");
-                    int temp = int.Parse(Console.ReadLine());
-                    if (temp == 0) { }//sync
-                    else//sync date
-                        break;
-                case Choices.Traveled:
-
-                    break;
-                case Choices.Exit:
-                    break;
-                default:
-                    break;
+                Console.WriteLine("Enter bus license plate: ");
+                string licensePlate = Console.ReadLine();
+                return licensePlate;
             }
-            choice = (int)ShowChoices();
+            List<Bus> BusList = new List<Bus>();
+            int choice = (int)ShowChoices();
+            while (choice != 0)
+            {
+                switch ((Choices)choice)
+                {
+                    case Choices.Add_Bus:
+                        try
+                        {
+                            Console.WriteLine("Enter bus starting date: ");
+                            var userInput = Console.ReadLine();
+                            DateTime Dateinput;
+                            DateTime.TryParse(userInput, out Dateinput);
+                            ///Check if the date makes sense (in 1896 Countries began to use a number of plates)
+                            if (!(Dateinput.Day>0 && Dateinput.Day<32) ||!(Dateinput.Month>0 && Dateinput.Month<13) ||!(Dateinput.Year>1895 && Dateinput.Year<2021))
+                            {
+                                Console.WriteLine("Wrong date");
+                                break;
+                            }
+                            Bus AddBus = new Bus();
+                            AddBus.DATEACTIVITY = Dateinput;
+                            string licenNumber = Licenseplate();
+                            ///input integrity check (-2 because license plates have 2 '-')
+                            if (licenNumber.Length - 2 != AddBus.NumberOflicensePlate())
+                            {
+                                Console.WriteLine("Error: You typed the wrong number of license plaste");
+                                break;
+                            }
+                            AddBus.LICENSEPLATE = licenNumber;
+                            ///Check if the bus is already in the system
+                            for (int i = 0; i < BusList.Count; i++)
+                            {
+                                if (BusList[i].LICENSEPLATE == licenNumber)
+                                    throw new ArithmeticException("The plate number is already in the system");
+                            }
+                            BusList.Add(AddBus);
+                            Console.WriteLine("Enter Number of kilometers");
+                            ///Absorption from the user number kilometers,
+                            ///and notification to the user if the bus needs Treatment or refueling
+                            float k;
+                            float.TryParse(Console.ReadLine(), out k);
+                            AddBus.KILOMETERSGAS = k;
+                            AddBus.Mileage(k);
+                            if (AddBus.TreatmentIsNeeded())
+                                Console.WriteLine("The Bus Needs A Treatment");
+                            if (AddBus.FuelCondition())
+                                Console.WriteLine("The Bus Needs A refuel");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("The plate number is already in the system");
+                        }
+                        break;
+                    case Choices.Choose_Bus:
+                        bool text = false;
+                        string input = Licenseplate();
+                        for (int i = 0; i < BusList.Count; i++)
+                        {
+                            if (BusList[i].LICENSEPLATE == input)
+                            {
+                                text = true;
+                                int save = Bus.r.Next(0, 1201);///Saveing the Random number
+                                ///Check if the bus is allowed to travel
+                                if (BusList[i].TreatmentIsNeeded())
+                                    Console.WriteLine("ERROR: Unable to make a trip, you must send for handling ");
+                                if (BusList[i].FuelCondition())
+                                    Console.WriteLine("ERROR: Unable to make a trip, you must send for refueling ");
+                                else
+                                {
+                                    ///Adds the miles
+                                    BusList[i].KILOMETERSGAS += save;
+                                    BusList[i].kILOMETERS_TREATMENT += save;
+                                    BusList[i].Mileage(BusList[i].KILOMETERSGAS);
+                                    Console.WriteLine("Bus traveled successfully");
+                                    break;
+                                }
+                            }
+                        }
+                        if (!text)
+                            Console.WriteLine("The bus does not exist in the system");
+                        break;
+                    case Choices.Refuel_Treat:
+                        string lnumber = Licenseplate();
+                        Console.WriteLine("Click 0 if you want to Refuel");
+                        Console.WriteLine("Click 1 if you want to Treat the bus");
+                        int temp;
+                        int.TryParse(Console.ReadLine(), out temp);
+                        bool text1 = false;
+                        if (temp == 0)
+                        {
+                            for (int i = 0; i < BusList.Count; i++)
+                            {
+                                if (BusList[i].LICENSEPLATE == lnumber)
+                                {
+                                    text1=true;
+                                    BusList[i].KILOMETERSGAS= 0;///Reset,because the problem is solved
+                                    Console.WriteLine("Fueling was done successfully");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < BusList.Count; i++)
+                            {
+                                if (BusList[i].LICENSEPLATE == lnumber)
+                                {
+                                    text1 = true;
+                                    BusList[i].DATETREATMET = currentDate;
+                                    BusList[i].kILOMETERS_TREATMENT = 0;///Reset,because the problem is solved
+                                    Console.WriteLine("Treatment was done successfully");
+
+                                }
+                            }
+
+                        }
+                        if(!text1)
+                            Console.WriteLine("The bus does not exist in the system");
+                        break;
+                    case Choices.Traveled:
+                        ///Prints all details of the buss 
+                        int counter = 0;
+                        for (int i = 0; i < BusList.Count; i++)
+                        {
+                            counter++;
+                            Console.WriteLine("Bus details {0}", counter);
+                            Console.WriteLine("number license plaste :{0}", BusList[i].LICENSEPLATE);
+                            Console.WriteLine("Date of ascent to the road :{0} ", BusList[i].DATEACTIVITY);
+                            Console.WriteLine("Date of last Treatment :{0} ", BusList[i].DATETREATMET);
+                            Console.WriteLine("Number of kilometers :{0}  ", BusList[i].kILOMETERS_TREATMENT);
+                        }
+                        break;
+                    case Choices.Exit:
+                    default:
+                        break;
+                }
+                choice = (int)ShowChoices();
+            }
         }
     }
-   }
 }
+/// <summary>
+/// output:Enter your choice of action:
+//Click 1 to add a bus
+//Click 2 to choosing a bus for travel
+//Click 3 to refuel or treat a bus
+//Click 4 to show travel of the last treatment for all vehicles in the company
+//Click 0 to exit the program
+//1
+//Enter bus starting date:
+//12 / 09 / 2017
+//Enter bus license plate:
+//12 - 345 - 56
+//Enter Number of kilometers
+//1300
+//Enter your choice of action:
+//Click 1 to add a bus
+//Click 2 to choosing a bus for travel
+//Click 3 to refuel or treat a bus
+//Click 4 to show travel of the last treatment for all vehicles in the company
+//Click 0 to exit the program
+//2
+//Enter bus license plate:
+//12 - 345 - 56
+//ERROR: Unable to make a trip, you must send for refueling
+//Enter your choice of action:
+//Click 1 to add a bus
+//Click 2 to choosing a bus for travel
+//Click 3 to refuel or treat a bus
+//Click 4 to show travel of the last treatment for all vehicles in the company
+//Click 0 to exit the program
+//3
+//Enter bus license plate:
+//12 - 345 - 56
+//Click 0 if you want to Refuel
+//Click 1 if you want to Treat the bus
+//0
+//Fueling was done successfully
+//Enter your choice of action:
+//Click 1 to add a bus
+//Click 2 to choosing a bus for travel
+//Click 3 to refuel or treat a bus
+//Click 4 to show travel of the last treatment for all vehicles in the company
+//Click 0 to exit the program
+//2
+//Enter bus license plate:
+//12 - 345 - 56
+//Bus traveled successfully
+//Enter your choice of action:
+//Click 1 to add a bus
+//Click 2 to choosing a bus for travel
+//Click 3 to refuel or treat a bus
+//Click 4 to show travel of the last treatment for all vehicles in the company
+//Click 0 to exit the program
+//4
+//Bus details 1
+//number license plaste :12 - 345 - 56
+//Date of ascent to the road :12 / 09 / 2017 00:00:00
+//Date of last Treatment :01 / 01 / 0001 00:00:00
+//Number of kilometers :125
+//Enter your choice of action:
+//Click 1 to add a bus
+//Click 2 to choosing a bus for travel
+//Click 3 to refuel or treat a bus
+//Click 4 to show travel of the last treatment for all vehicles in the company
+//Click 0 to exit the program
+/// </summary>
