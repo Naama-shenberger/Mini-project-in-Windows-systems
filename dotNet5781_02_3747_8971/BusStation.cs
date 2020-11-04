ce129950ccda72838262a1ca777a1adb349f04f6
ella hanzin <hanzin@g.jct.ac.il>
2020-11-04 16:51:10 +0200

    hey!

---
+++ dotNet5781_02_3747_8971/BusStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_3747_8971
+{
+    class BusStation
+    {
+        private string  BusCode;
+        protected double Latitude, Longitude;
+
+        //public static  place(double latitude,double longitude)
+        //{
+        //    this.Latitude = latitude;
+        //    this.Longitude = longitude;
+        //}
+        public string BUSCODE
+        {
+            get { return BusCode; }
+            set { BusCode = value; }
+        }
+    }
+}
---
+++ dotNet5781_02_3747_8971/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_3747_8971
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
