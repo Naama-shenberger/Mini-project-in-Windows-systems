c1b17a846a15b4b3845ab22c782473e05334acd7
נעמה שנברגר <shanberg@g.jct.ac.il>
2020-11-04 18:41:55 +0200

    NAAMA

---
+++ dotNet5781_02_3747_8971/BusLineStation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_3747_8971
+{
+    class BusLineStation: BusStation
+    {
+       public double DistanceBusLineStation(BusLineStation previous)
+       {
+            this.Latitude = previous.Latitude - this.Latitude; ///קו רוחב 
+            this.Longitude = previous.Longitude - this.Longitude;//קו אורך
+       }
+       public DateTime TravelTime(BusLineStation previous)
+       {
+
+       }
+    }
+}
---
+++ dotNet5781_02_3747_8971/SingleBusLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_3747_8971
+{
+    //enum 
+    class SingleBusLine : BusLineStation
+    {
+        protected List<BusLineStation> Stations = new List<BusLineStation>();//Define a list of bus line stops
+        int BusLine;//number line
+        BusLineStation FirstStation;//Departure station
+        BusLineStation LastStation;//terminus
+        public override string ToString()
+        {
+
+            return base.ToString();
+        }
+
+
+    }
+}
