9643e7528e61cd3cbdc56e3c0f5b92a7be4c7a1c
ella hanzin <hanzin@g.jct.ac.il>
2021-01-10 19:22:07 +0200

    חלונות חדשות

---
+++ PL.WPF/TimeUpdateWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace PL.WPF
+{
+    /// <summary>
+    /// Interaction logic for TimeUpdateWindow.xaml
+    /// </summary>
+    public partial class TimeUpdateWindow : Window
+    {
+        TimeSpan timedrive;
+        public TimeUpdateWindow()
+        {
+            InitializeComponent();
+        }
+        public TimeSpan Timedrive
+        {
+            get { return timedrive; }
+        }
+        /// <summary>
+        ///event Done Click 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            timedrive = TimeSpan.Parse(Regex.Replace(Timepicker.Text, "[A-Za-z ]", ""));
+            this.Close();
+        }
+    }
+}
