c96d186c3f99d5885c8ad19c88ba4ad4ddd1c555
נעמה שנברגר <shanberg@g.jct.ac.il>
2021-01-13 22:35:22 +0200

    synce

---
+++ DalXml/BO/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BO
+{
+    public class Enums
+    {
+        /// <summary>
+        /// enum of Zones for bus lines
+        /// </summary>
+        public enum Zones { Itamar, Zeev_hill, Alon_Shvut, Gush_Dan, Gush_Etzion, Galilee, Jerusalem_Corridor, Beer_Sheva, General, Zefat, Ramat_Gan }
+        public enum Status { Ready_to_go, In_the_middle_of_A_ride, refueling, In_treatment, Dangerous }
+    }
+}
---
+++ DalXml/BO/LineRides.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class LineRides
+    {
+        /// <summary>
+        /// sets and gets
+        /// </summary> 
+        public bool Active { set; get; }// status of a bus line whether it is active or not
+        public TimeSpan TravelStartTime { set; get; }//Travel start time of the bus 
+        public TimeSpan TravelEndTime { set; get; }//Travel end time
+        public TimeSpan BusDepartureNumber { set; get; }//Exit number of the line
+        public int ID { set; get; }
+    }
+}
